1d27b4a41e171737d133dd51c5979e38ed963fe6
mkl04 <mjcampost4196@gmail.com>
2023-08-15 10:46:00 -0500

    update training and evaluation for MMD

--- models/at_unet_mmd.py
+++ models/at_unet_mmd.py
@@ -18,17 +18,29 @@ def conv2d_block(in_channels, out_channels):
 
 
 class Encoder(nn.Module):
-    def __init__(self, f1=16):
+    def __init__(self, f1=16, depth=6, mode="cascade"):
         super(Encoder, self).__init__()
+        
+        self.mode = mode
+        self.depth = depth
         
         self.dconv1 = conv2d_block(1, f1)
         self.dconv2 = conv2d_block(f1, f1*2)
         self.dconv3 = conv2d_block(f1*2, f1*4)
         
         self.maxpool = nn.MaxPool2d(kernel_size=2)
-        # self.dropout = nn.Dropout()
+#         self.dropout = nn.Dropout()
 
-        self.bottle = conv2d_block(f1*4, f1*8)
+#         self.bottle = conv2d_block(f1*4, f1*8)
+        
+        nf_bottle = f1*8
+        self.bottle1 = nn.Conv2d(f1*4, nf_bottle, kernel_size=3, stride=1, padding=1)
+        self.bottle2 = nn.Conv2d(nf_bottle, nf_bottle, kernel_size=3, stride=1, padding=2, dilation=2)
+        self.bottle3 = nn.Conv2d(nf_bottle, nf_bottle, kernel_size=3, stride=1, padding=4, dilation=4)
+        self.bottle4 = nn.Conv2d(nf_bottle, nf_bottle, kernel_size=3, stride=1, padding=8, dilation=8)
+        self.bottle5 = nn.Conv2d(nf_bottle, nf_bottle, kernel_size=3, stride=1, padding=16, dilation=16)
+        self.bottle6 = nn.Conv2d(nf_bottle, nf_bottle, kernel_size=3, stride=1, padding=32, dilation=32)
+        self.bottle_list = [self.bottle1, self.bottle2, self.bottle3, self.bottle4, self.bottle5, self.bottle6]
 
     def forward(self, x):
 
@@ -40,9 +52,20 @@ class Encoder(nn.Module):
         
         conv3 = self.dconv3(x2)
         x3 = self.maxpool(conv3)
-        # x3 = self.dropout(x3)
+#         x3 = self.dropout(x3)
         
-        enc_out = self.bottle(x3)       
+#         enc_out = self.bottle(x3)
+
+        bottle = self.bottle_list[0](x3)
+        
+        dilated_layers = []
+        dilated_layers.append(bottle)
+        if self.mode == "cascade":
+            for i in range(self.depth-1):
+                bottle = self.bottle_list[i+1](bottle)
+                dilated_layers.append(bottle)
+        
+        enc_out = sum(dilated_layers)
         
         return enc_out, conv3, conv2, conv1
 
@@ -79,22 +102,24 @@ class Decoder(nn.Module):
         class_output = self.conv_last(conv_1)
         class_output = torch.sigmoid(class_output)
         
-        return class_output, conv_1, conv_2, conv_3 
+        return class_output   
 
-class UNet2D(nn.Module):
+
+class AtrousUNet(nn.Module):
     """
-    UNet architecture for MMD
+    Atrous UNet architecture for MMD
 
     Attributes:
         f1 (int): number of filters in the first convolutional layers
+        depth (int): number of atrous convolutional layers
+        mode (str): exists two modes, but just one was implemented in pytorch
         branches (list): list of branches to compute MMD
-        
     """
-    def __init__(self, f1=16, branches=['bottle']):
-        super(UNet2D, self).__init__()
-        self.encoder = Encoder(f1)
+    def __init__(self, f1=16, depth=6, mode="cascade", branches=['bottle']):
+        super(AtrousUNet, self).__init__()
+        self.encoder = Encoder(f1, depth=depth, mode=mode)
         self.decoder = Decoder(f1)
-
+        
         self.branches = branches
 
         if 'conv3' in branches:
@@ -112,37 +137,12 @@ class UNet2D(nn.Module):
         if 'bottle_v2' in branches:
             # 128, 16x16
             self.conv_db  = nn.Conv2d(128, 4, kernel_size=3, padding=1)
-        
-        if 'conv_3' in branches:
-            # 64, 32x32
-            self.conv_d_3 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
             
-        if 'conv_3_v2' in branches:
-            # 64, 32x32
-            self.conv_d_3 = nn.Conv2d(64, 4, kernel_size=3, padding=1)
-            
-        if 'conv_2' in branches:
-            # 32, 
-            self.conv_d_2 = nn.Conv2d(32, 16, kernel_size=3, padding=1)
-            
-        if 'conv_2_v2' in branches:
-            # 32, 
-            self.conv_d_2 = nn.Conv2d(32, 1, kernel_size=3, padding=1)
-            
-        if 'conv_1_v1' in branches:
-            # 16, 
-            self.conv_d_1 = nn.Conv2d(16, 8, kernel_size=3, padding=1)
-            
-        if 'conv_1_v2' in branches:
-            # 16, 
-            self.conv_d_1 = nn.Conv2d(16, 1, kernel_size=3, padding=1)
-
         self.maxpool = nn.MaxPool2d(kernel_size=2)
 
     def forward(self, x):
         enc_out, conv3, conv2, conv1 = self.encoder(x)
         dec_out = self.decoder(enc_out, conv3, conv2, conv1)
-        dec_out, conv_1, conv_2, conv_3 = self.decoder(enc_out, conv3, conv2, conv1)
         
         features = []
         for branch_name in self.branches:
@@ -156,25 +156,7 @@ class UNet2D(nn.Module):
             if 'bottle' in branch_name:
                 branch = self.conv_db(enc_out)
                 branch = self.maxpool(branch)
-
-                features.append(branch.view(branch.size(0), -1)) # Flatten
                 
-            if 'conv_3' in branch_name:
-                branch = self.conv_d_3(conv_3)
-                branch = self.maxpool(branch)
-
-                features.append(branch.view(branch.size(0), -1)) # Flatten
-                
-            if 'conv_2' in branch_name:
-                branch = self.conv_d_2(conv_2)
-                branch = self.maxpool(branch)
-
-                features.append(branch.view(branch.size(0), -1)) # Flatten
-                
-            if 'conv_1' in branch_name:
-                branch = self.conv_d_1(conv_1)
-                branch = self.maxpool(branch)
-
                 features.append(branch.view(branch.size(0), -1)) # Flatten
                 
         return dec_out, features
